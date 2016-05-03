4f76fe97e692b74d54dbd33ab3aa19db92a9a9ac
Jose Troche <jose.troche.coder@gmail.com>
2016-05-03 19:32:38 -0400

    Added AWS IoT logic and initial drone commands

--- androidApp/app/src/main/java/com/jose_troche/alexadronepilot/MainActivity.java
+++ androidApp/app/src/main/java/com/jose_troche/alexadronepilot/MainActivity.java
@@ -2,6 +2,7 @@ package com.jose_troche.alexadronepilot;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +10,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.iot.AWSIotKeystoreHelper;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttClientStatusCallback;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttManager;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttNewMessageCallback;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttQos;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.iot.AWSIotClient;
+import com.amazonaws.services.iot.model.AttachPrincipalPolicyRequest;
+import com.amazonaws.services.iot.model.CreateKeysAndCertificateRequest;
+import com.amazonaws.services.iot.model.CreateKeysAndCertificateResult;
 import com.jose_troche.alexadronepilot.parrot.DroneListActivity;
 import com.jose_troche.alexadronepilot.parrot.MiniDrone;
 
@@ -17,20 +30,69 @@ import com.parrot.arsdk.arcommands.ARCOMMANDS_MINIDRONE_PILOTINGSTATE_FLYINGSTAT
 import com.parrot.arsdk.arcontroller.ARCONTROLLER_DEVICE_STATE_ENUM;
 import com.parrot.arsdk.ardiscovery.ARDiscoveryDeviceService;
 
+import java.io.UnsupportedEncodingException;
+import java.security.KeyStore;
+import java.util.UUID;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
+    // Drone Variables
     private MiniDrone mMiniDrone;
-
     private ProgressDialog mConnectionProgressDialog;
-    private ProgressDialog mDownloadProgressDialog;
-
     private TextView mBatteryLabel;
     private Button mTakeOffLandBt;
+
+    // --- AWS IoT Constants to modify per your configuration ---
+
+    // Endpoint Prefix = random characters at the beginning of the custom AWS
+    // IoT endpoint
+    // describe endpoint call returns: XXXXXXXXXX.iot.<region>.amazonaws.com,
+    // endpoint prefix string is XXXXXXX
+    private static final String CUSTOMER_SPECIFIC_ENDPOINT_PREFIX = "A3RR56M9D36ZD1";
+    // Cognito pool ID. For this app, pool needs to be unauthenticated pool with
+    // AWS IoT permissions.
+    private static final String COGNITO_POOL_ID = "us-east-1:1e1220dd-14b8-4456-9374-6ff6593195ef";
+    // Name of the AWS IoT policy to attach to a newly created certificate
+    private static final String AWS_IOT_POLICY_NAME = "FullAccess";
+
+    // Region of AWS IoT
+    private static final Regions MY_REGION = Regions.US_EAST_1;
+    // Filename of KeyStore file on the filesystem
+    private static final String KEYSTORE_NAME = "iot_keystore";
+    // Password for the private key in the KeyStore
+    private static final String KEYSTORE_PASSWORD = "jnUSZX8GWHh2kr9pwTDWLV9V";
+    // Certificate and key aliases in the KeyStore
+    private static final String CERTIFICATE_ID = "default";
+    // The IoT Drone Topic
+    private static final String MQTT_TOPIC = "$aws/things/Drone/shadow/update";
+
+    // AWS IoT Variables
+    private TextView iotCommand;
+    private TextView iotStatus;
+
+    private AWSIotClient mIotAndroidClient;
+    private AWSIotMqttManager mqttManager;
+
+    private String clientId;
+    private String keystorePath;
+    private String keystoreName;
+    private String keystorePassword;
+    private String certificateId;
+    private KeyStore clientKeyStore = null;
+
+    private CognitoCachingCredentialsProvider credentialsProvider;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initAwsIot();
 
         initIHM();
 
@@ -63,6 +125,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
+        mqttManager.disconnect();
+
         if (mMiniDrone != null)
         {
             mConnectionProgressDialog = new ProgressDialog(this, R.style.AppCompatAlertDialogStyle);
@@ -77,6 +141,229 @@ public class MainActivity extends AppCompatActivity {
         } else {
             finish();
         }
+    }
+
+    private void initAwsIot() {
+        iotCommand = (TextView) findViewById(R.id.iotMessage);
+        iotStatus =  (TextView) findViewById(R.id.iotStatus);
+
+        // MQTT client IDs are required to be unique per AWS IoT account.
+        clientId = UUID.randomUUID().toString();
+
+        // Initialize the AWS Cognito credentials provider
+        credentialsProvider = new CognitoCachingCredentialsProvider(
+            getApplicationContext(), // context
+            COGNITO_POOL_ID, // Identity Pool ID
+            MY_REGION // Region
+        );
+
+        Region region = Region.getRegion(MY_REGION);
+
+        // MQTT Client
+        mqttManager = new AWSIotMqttManager(clientId, region, CUSTOMER_SPECIFIC_ENDPOINT_PREFIX);
+
+        // Set keepalive to 10 seconds.  Will recognize disconnects more quickly but will also send
+        // MQTT pings every 10 seconds.
+        mqttManager.setKeepAlive(10);
+
+        // IoT Client (for creation of certificate if needed)
+        mIotAndroidClient = new AWSIotClient(credentialsProvider);
+        mIotAndroidClient.setRegion(region);
+
+        keystorePath = getFilesDir().getPath();
+        keystoreName = KEYSTORE_NAME;
+        keystorePassword = KEYSTORE_PASSWORD;
+        certificateId = CERTIFICATE_ID;
+
+        loadClientKeyStore();
+
+        if (clientKeyStore == null) {
+            createPrivateKeyAndCertificate();
+        }
+
+        connectToIoT();
+
+    }
+
+    private void sendCommand(String command){
+        Handler handler = new Handler();
+        iotCommand.setText(command);
+
+        switch (command.toLowerCase()){
+            case "take off":
+                mMiniDrone.takeOff();
+                break;
+            case "land":
+                mMiniDrone.land();
+                break;
+            case "up":
+                mMiniDrone.setGaz((byte) 50);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mMiniDrone.setGaz((byte) 0);
+                    }
+                }, 1000);
+                break;
+            case "down":
+                mMiniDrone.setGaz((byte) -50);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mMiniDrone.setGaz((byte) 0);
+                    }
+                }, 1000);
+                break;
+        }
+
+    }
+
+    private void subscribeToTopic(){
+        Log.d(TAG, "Mqtt topic = " + MQTT_TOPIC);
+
+        try {
+            mqttManager.subscribeToTopic(MQTT_TOPIC, AWSIotMqttQos.QOS0,
+                new AWSIotMqttNewMessageCallback() {
+                    @Override
+                    public void onMessageArrived(final String topic, final byte[] data) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                            try {
+                                String message = new String(data, "UTF-8");
+                                String command = (new JSONObject(message)).getString("command");
+                                Log.d(TAG, "Command arrived:");
+                                Log.d(TAG, "   Topic: " + topic);
+                                Log.d(TAG, " Command: " + command);
+
+                                sendCommand(command);
+
+                            } catch (UnsupportedEncodingException e) {
+                                Log.e(TAG, "Message encoding error.", e);
+                            } catch (JSONException e) {
+                                Log.e(TAG, "JSON Message decoding error.", e);
+                            }
+                            }
+                        });
+                    }
+                });
+        } catch (Exception e) {
+            Log.e(TAG, "Subscription error.", e);
+        }
+    }
+
+    private void connectToIoT(){
+        Log.d(TAG, "clientId = " + clientId);
+
+        try {
+            mqttManager.connect(clientKeyStore, new AWSIotMqttClientStatusCallback() {
+                @Override
+                public void onStatusChanged(final AWSIotMqttClientStatus status,
+                                            final Throwable throwable) {
+                    Log.d(TAG, "Status = " + String.valueOf(status));
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                        if (status == AWSIotMqttClientStatus.Connecting) {
+                            iotStatus.setText("Connecting...");
+
+                        } else if (status == AWSIotMqttClientStatus.Connected) {
+                            iotStatus.setText("Connected");
+                            subscribeToTopic();
+                        } else if (status == AWSIotMqttClientStatus.Reconnecting) {
+                            if (throwable != null) {
+                                Log.e(TAG, "Connection error.", throwable);
+                            }
+                            iotStatus.setText("Reconnecting");
+                        } else if (status == AWSIotMqttClientStatus.ConnectionLost) {
+                            if (throwable != null) {
+                                Log.e(TAG, "Connection error.", throwable);
+                            }
+                            iotStatus.setText("Disconnected");
+                        } else {
+                            iotStatus.setText("Disconnected");
+                        }
+                        }
+                    });
+                }
+            });
+        } catch (final Exception e) {
+            Log.e(TAG, "Connection error.", e);
+            iotStatus.setText("Error! " + e.getMessage());
+        }
+    }
+
+    private void loadClientKeyStore(){
+        try {
+            if (AWSIotKeystoreHelper.isKeystorePresent(keystorePath, keystoreName)) {
+                if (AWSIotKeystoreHelper.keystoreContainsAlias(certificateId, keystorePath,
+                        keystoreName, keystorePassword)) {
+                    Log.i(TAG, "Certificate " + certificateId
+                            + " found in keystore - using for MQTT.");
+                    // load keystore from file into memory to pass on connection
+                    clientKeyStore = AWSIotKeystoreHelper.getIotKeystore(certificateId,
+                            keystorePath, keystoreName, keystorePassword);
+                } else {
+                    Log.i(TAG, "Key/cert " + certificateId + " not found in keystore.");
+                }
+            } else {
+                Log.i(TAG, "Keystore " + keystorePath + "/" + keystoreName + " not found.");
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "An error occurred retrieving cert/key from keystore.", e);
+        }
+    }
+
+    // Create a key and certificate, and store them in key store
+    private void createPrivateKeyAndCertificate(){
+        Log.i(TAG, "Cert/key was not found in keystore - creating new key and certificate.");
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+            try {
+                // Create a new private key and certificate. This call
+                // creates both on the server and returns them to the
+                // device.
+                CreateKeysAndCertificateRequest createKeysAndCertificateRequest =
+                        new CreateKeysAndCertificateRequest();
+                createKeysAndCertificateRequest.setSetAsActive(true);
+                final CreateKeysAndCertificateResult createKeysAndCertificateResult;
+                createKeysAndCertificateResult =
+                        mIotAndroidClient.createKeysAndCertificate(createKeysAndCertificateRequest);
+                Log.i(TAG, "Cert ID: " +
+                        createKeysAndCertificateResult.getCertificateId() + " created.");
+
+                // store in keystore for use in MQTT client
+                // saved as alias "default" so a new certificate isn't
+                // generated each run of this application
+                AWSIotKeystoreHelper.saveCertificateAndPrivateKey(certificateId,
+                        createKeysAndCertificateResult.getCertificatePem(),
+                        createKeysAndCertificateResult.getKeyPair().getPrivateKey(),
+                        keystorePath, keystoreName, keystorePassword);
+
+                // load keystore from file into memory to pass on
+                // connection
+                clientKeyStore = AWSIotKeystoreHelper.getIotKeystore(certificateId,
+                        keystorePath, keystoreName, keystorePassword);
+
+                // Attach a policy to the newly created certificate.
+                // This flow assumes the policy was already created in
+                // AWS IoT and we are now just attaching it to the
+                // certificate.
+                AttachPrincipalPolicyRequest policyAttachRequest =
+                        new AttachPrincipalPolicyRequest();
+                policyAttachRequest.setPolicyName(AWS_IOT_POLICY_NAME);
+                policyAttachRequest.setPrincipal(createKeysAndCertificateResult
+                        .getCertificateArn());
+                mIotAndroidClient.attachPrincipalPolicy(policyAttachRequest);
+
+            } catch (Exception e) {
+                Log.e(TAG, "Exception occurred when generating new private key and certificate.", e);
+            }
+            }
+        }).start();
     }
 
     private void initIHM() {
@@ -165,5 +452,7 @@ public class MainActivity extends AppCompatActivity {
         public void onDownloadProgressed(String mediaName, int progress) {}
 
     };
+
+
 
 }
